8edb6c794ee14ebaf999352ae4f5a57eb29a4ddb
shishiranchan <shishiranchan33@gmail.com>
2025-03-01 12:02:35 +0530

    i did changes in error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 
+
 export default function Error() {
   return(
    <div>
+    <img src="./imagehelo.jpg">alt="hello git new branch 01-03-2005-h"</img>
     
+           
     404 page not found
    </div>
   );
